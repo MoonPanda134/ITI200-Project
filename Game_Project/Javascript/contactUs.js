cbc68dddad73b2bed5e36a08fbaa4f6bb58edaae
MoonPanda134 <93020315+MoonPanda134@users.noreply.github.com>
2024-10-14 18:51:33 -0400

    Added Contact Html

---
+++ Game_Project/Javascript/contactUs.js
@@ -0,0 +1,6 @@
+var submitForm = document.getElementById("Submit")
+
+function doThis(){
+    alert("Thank you for your feedback! We will get back to you soon!")
+}
+submitForm.addEventListener("click", doThis)
---
+++ Game_Project/Javascript/intro-1.js
@@ -0,0 +1,15 @@
+var dialog = document.getElementById("dialog")
+
+let txtline = 0
+
+let txt = [
+    "Hello Adventurer....",
+    "hey",
+    "hi",
+    " ",
+    " "
+]
+
+function next(txtline){
+    display.innerText = text[txtline]
+}
