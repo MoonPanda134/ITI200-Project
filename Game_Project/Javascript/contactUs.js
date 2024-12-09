3eade1c99657657cb665e6ef94ddf8669666c8ac
SRKrupp <srkrupp@umich.edu>
2024-12-09 12:09:39 -0500

    added some information for postgres
    Having trouble getting the functionality I want out of node, very frustrating

--- Game_Project/Javascript/contactUs.js
+++ Game_Project/Javascript/contactUs.js
@@ -4,3 +4,24 @@ function doThis(){
     alert("Thank you for your feedback! We will get back to you soon!")
 }
 submitForm.addEventListener("click", doThis)
+
+const form = document.getElementById("contact-information");
+
+async function saveData(event){
+  event.preventDefault();
+  const formEmail = document.getElementById("emailInfo");
+  const formMessage = document.getElementById("messageInfo");
+  const contactinfo = {
+    email: formEmail.value,
+    message: formMessage.value
+  };
+  await fetch('/api/contact/save', {
+       headers: {
+           'Accept': 'application/json',
+           'Content-Type': 'application/json'
+       },
+       method: 'POST',
+       body: JSON.stringify(product)
+   });
+}
+form.addEventListener("Submit", saveData)
